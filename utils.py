076d51ac6c540b41d942f5be1748db99452bf134
布苓苓苓 <80634381+Brynn-pl@users.noreply.github.com>
2023-03-18 20:09:03 +0800

    from scipy.misc import imread,imresize

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import h5py
 import json
 import torch
-from scipy.misc import imresize
+#from scipy.misc import imread,imresize
 from PIL import Image
 from tqdm import tqdm
 from collections import Counter
@@ -118,7 +118,8 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
                 if len(img.shape) == 2:
                     img = img[:, :, np.newaxis]
                     img = np.concatenate([img, img, img], axis=2)
-                img = imresize(img, (256, 256))
+                #img = imresize(img, (256, 256))
+                img = Image.resize(img, (256, 256))
                 img = img.transpose(2, 0, 1)
                 assert img.shape == (3, 256, 256)
                 assert np.max(img) <= 255
